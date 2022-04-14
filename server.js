329f4ea4dec4a5a640b684cb9dfb68abdfaa2aae
leroy-netizen <leroythomas051@gmail.com>
2022-04-14 17:31:18 +0300

    refactor routes by by introducing controllers

--- server.js
+++ server.js
@@ -1,43 +1,18 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+
+//route files
+import bootcampRoutes from './routes/bootcampRoutes.js';
 
 // load env vars
-
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//Mount routers
+app.use('/api/v1/bootcamps', bootcampRoutes);
+
 const PORT = process.env.PORT || 8000;
-
-// -------------routes-------------
-
-//`````POST``````
-app.post("/api/v1/bootcamps/:id", (req, res) => {
-  res.json({ success: true, message: "create a bootcamp" });
-});
-
-//`````GET``````
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.json({ success: true, message: "get all bootcamps" });
-});
-
-// get a single bootcamp
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-  res.json({
-    success: true,
-    message: `get a single bootcamp ${req.params.id}`,
-  });
-});
-
-//`````update{PUT}``````
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-  res.json({ success: true, message: `update a bootcamp ${req.params.id}` });
-});
-
-//`````DELETE``````
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-  res.json({ success: true, message: `delete a bootcamp ${req.params.id}` });
-});
 
 app.listen(
   PORT,
